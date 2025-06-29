521bc2b64ff3836bfff528d50c5d0db0d2062760
Thuathe <raifuteika15@gmail.com>
2025-06-29 15:37:40 +0700

    coba tulisan

--- src/components/Homes/Hero.jsx
+++ src/components/Homes/Hero.jsx
@@ -6,11 +6,11 @@ const Hero = () => {
   return (
 <section 
   id="Beranda" 
-  className="relative w-full h-auto aspect-[20/15] md:aspect-auto md:h-screen overflow-hidden bg-black flex justify-center"
+  className="relative w-full h-auto aspect-[10/11] md:aspect-auto md:h-screen overflow-hidden bg-black flex justify-center"
 >
   {/* Background Image */}
   <img 
-    src="https://res.cloudinary.com/dq0kdch2r/image/upload/v1751185553/bg_ugpb45.jpg" 
+    src="\public\images\bg.jpg" 
     alt="Background" 
     className="absolute top-0 left-0 object-cover w-full h-full object-[70%] md:object-center" 
   />
@@ -23,7 +23,7 @@ const Hero = () => {
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1 }}
-      className="mt-[50px] md:mt-0 text-xl md:text-4xl lg:text-[45px] tracking-wider font-bold text-black font-satoshi leading-[20px] md:leading-[45px]"
+      className="mt-[40px] md:mt-0 text-[35px] md:text-4xl lg:text-[45px] tracking-wider font-bold text-black font-satoshi leading-[35px] md:leading-[45px]"
     >
       Temukan Website<br />Temukan Cerita Dibaliknya
     </motion.h1>
@@ -33,7 +33,7 @@ const Hero = () => {
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1, delay: 0.3 }}
-      className="text-[8px] md:text-base lg:text-[13.5px] italic text-black font-poppins"
+      className="text-[12px] md:text-base lg:text-[13.5px] italic text-black font-poppins"
     >
       "Galeri digital tempat website kami hidup dan bercerita. <br />
       Jelajahi karya, temukan inspirasi di setiap halaman."
@@ -46,13 +46,13 @@ const Hero = () => {
       transition={{ duration: 0.1, delay: 0.5 }}
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
-      className="relative inline-flex items-center justify-center px-[10px] py-[6px] space-x-2 overflow-hidden text-[8px] font-medium text-black transition-all duration-300 border rounded-lg cursor-pointer md:px-8 md:py-3 md:text-base bg-white/30 border-invaPurple font-poppins group"
+      className="relative inline-flex items-center justify-center px-[10px] py-[6px] space-x-2 overflow-hidden text-[12px] font-medium text-black transition-all duration-300 border rounded-lg cursor-pointer md:px-8 md:py-3 md:text-base bg-white/30 border-invaPurple font-poppins group"
     >
       {/* Ripple Fill Effect */}
       <span className="absolute inset-0 w-0 h-0 bg-invaPurple opacity-20 rounded-full group-hover:w-72 group-hover:h-72 group-hover:opacity-40 transition-all duration-700 ease-out transform group-hover:scale-150 drop-shadow-[0_0_8px_#FF4FCB]"></span>
 
       {/* Icon */}
-      <ArrowRight className="relative z-10 transition-all duration-300 size-[10px] md:size-[20px] group-hover:text-invaGray text-invaPurple" />
+      <ArrowRight className="relative z-10 transition-all duration-300 size-[12px] md:size-[20px] group-hover:text-invaGray text-invaPurple" />
 
       {/* Button Text */}
       <span className="relative z-10 transition-all duration-300 animate-text-gradient group-hover:text-invaGray">

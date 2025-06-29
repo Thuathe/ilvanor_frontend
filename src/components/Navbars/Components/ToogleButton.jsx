9a0ad584c6d4b8fa1abe4616b2ed127f38b68341
Thuathe <raifuteika15@gmail.com>
2025-06-29 15:51:22 +0700

    tooglr

--- src/components/Navbars/Components/ToogleButton.jsx
+++ src/components/Navbars/Components/ToogleButton.jsx
@@ -5,9 +5,9 @@ const ToggleButton = ({ isOpen, toggleMenu }) => {
   return (
     <div className="md:hidden z-[999] cursor-pointer" onClick={toggleMenu}>
       {isOpen ? (
-        <X  className="w-4 h-4 text-black transition-transform duration-300" />
+        <X  className="text-black transition-transform duration-300 w-7 h-7" />
       ) : (
-        <MoreVertical  className="w-4 h-4 text-black transition-transform duration-300" />
+        <MoreVertical  className="text-black transition-transform duration-300 w-7 h-7" />
       )}
     </div>
   );
